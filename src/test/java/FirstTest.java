acc511b16668a079d5394f3802b110bc67cfff64
Sri Ganesh <sriganesh.d@i-exceed.com>
2022-06-02 10:33:14 +0530

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,35 @@
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+
+import javax.sql.rowset.serial.SerialJavaObject;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import java.util.*;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false));
+            BrowserContext context = browser.newContext(new Browser.NewContextOptions().setIgnoreHTTPSErrors(true));
+
+            Page page = context.newPage();
+
+
+            page.navigate("https://192.168.1.112:8443/BankAdmin/");
+            page.locator("[id=Admin__Login__userId]").fill("admin");
+//            page.pause();
+            page.locator("[id=Admin__Login__pswd]").fill("Admin@123");
+            page.locator("[id=Admin__Login__element_button_1]").click();
+            page.locator("[id=Admin__Dashboard__el_txt_1_txtcnt]").waitFor();
+
+            assertThat(page.locator("[id=Admin__Dashboard__el_txt_1_txtcnt]")).containsText("Dashboard");
+            // Press Enter
+            // page.waitForNavigation(new Page.WaitForNavigationOptions().setUrl("https://accounts.google.com/signin/v2/deniedsigninrejected?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F%3Fgws_rd%3Dssl&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin"), () ->
+//            page.waitForNavigation(() -> {
+//                page.locator("[id=]").press("Enter");
+//            });
+        }
+    }
+}
+
