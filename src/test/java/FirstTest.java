c679484f1185f6836777aff809d0d7e7f23fa802
sriganesh.d <sriganesh.d@i-exceed.com>
2023-01-02 16:56:04 +0530

    playwright poc

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -13,7 +13,7 @@ public class FirstTest {
                     .setHeadless(false));
             BrowserContext context = browser.newContext(new Browser.NewContextOptions().setIgnoreHTTPSErrors(true));
 
-            Page page = context.newPage();
+            Page page = browser.newPage();
 
 
             page.navigate("https://192.168.1.112:8443/BankAdmin/");
@@ -22,13 +22,12 @@ public class FirstTest {
             page.locator("[id=Admin__Login__pswd]").fill("Admin@123");
             page.locator("[id=Admin__Login__element_button_1]").click();
             page.locator("[id=Admin__Dashboard__el_txt_1_txtcnt]").waitFor();
-
             assertThat(page.locator("[id=Admin__Dashboard__el_txt_1_txtcnt]")).containsText("Dashboard");
             // Press Enter
             // page.waitForNavigation(new Page.WaitForNavigationOptions().setUrl("https://accounts.google.com/signin/v2/deniedsigninrejected?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F%3Fgws_rd%3Dssl&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin"), () ->
-//            page.waitForNavigation(() -> {
-//                page.locator("[id=]").press("Enter");
-//            });
+            page.waitForNavigation(() -> {
+                page.locator("[id=]").press("Enter");
+            });
         }
     }
 }
